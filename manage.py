844d77098ccf6aeec2b12db7ec2d0504f8738854
ShayvChan <511569025@qq.com>
2018-04-17 16:53:30 +0800

    redis配置

--- manage.py
+++ manage.py
@@ -1,12 +1,15 @@
 # -*-coding:utf-8-*-
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+import redis
 
 
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = 'mysql://root:fuckyou@127.0.0.1:3306/ihome'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    REDIS_HOST = '127.0.0.1'
+    REDIS_PORT = 6379
 
 
 app = Flask(__name__)
@@ -15,6 +18,9 @@ app.config.from_object(Config)
 
 # 实例化数据库
 db = SQLAlchemy(app)
+
+# 实例化redis
+redis_store = redis.StrictRedis(host=Config.REDIS_HOST, port=Config.REDIS_PORT)
 
 
 @app.route('/')
