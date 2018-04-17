e11c7ee5b63bfb32686dfc6327be46fc21136a6e
ShayvChan <511569025@qq.com>
2018-04-17 16:49:35 +0800

    mysql配置

--- manage.py
+++ manage.py
@@ -1,13 +1,20 @@
 # -*-coding:utf-8-*-
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 
 class Config(object):
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'mysql://root:fuckyou@127.0.0.1:3306/ihome'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 app = Flask(__name__)
+# 读取配置
 app.config.from_object(Config)
+
+# 实例化数据库
+db = SQLAlchemy(app)
 
 
 @app.route('/')
